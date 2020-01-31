2885bfb4b3631858bb82dbfc7fe9934c86e02b82
yuyang <447395536@qq.com>
2020-02-01 01:59:59 +0800

    实现计划 9.0 线程池 ThreadPoolExample

---
+++ src/main/java/com/mmall/concurrency/example/threadPool/ThreadPoolExample1.java
@@ -0,0 +1,32 @@
+package com.mmall.concurrency.example.threadPool;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author YYDCYY
+ * @create 2020-02-01
+ * Executors.newCachedThreadPool 演示
+ */
+@Slf4j
+public class ThreadPoolExample1 {
+    public static void main(String[] args) {
+        /**
+         * 放置 10 个任务,  
+         */
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    log.info("task : {}", index);
+                }
+            });
+        }
+        executorService.shutdown(); // 关闭线程池, 不然不会关闭.
+    }
+}
